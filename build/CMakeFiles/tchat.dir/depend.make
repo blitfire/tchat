f65bd7becc6e002c7405443bb5c9b934d5c9a930
George Hill <georgehill7924@gmail.com>
2025-02-13 00:40:30 +0000

    Initial commit.

---
+++ build/CMakeFiles/tchat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tchat.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/tchat.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/tchat.dir/main.cpp.o: /home/george/vim-projects/tchat/main.cpp \
+ /usr/include/stdc-predef.h
