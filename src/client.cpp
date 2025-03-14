78c053c710a781db3c6f0fda0efd32b62c6bd2e4
George Hill <georgehill7924@gmail.com>
2025-03-14 12:49:40 +0000

    Working client/server setup for client sending messages to the server

--- src/client.cpp
+++ src/client.cpp
@@ -1,23 +1,56 @@
-#include <string>
 #include <iostream>
 #include <asio.hpp>
 
 using asio::ip::tcp;
 
-int main(int argc, char* argv[]) {
+void wait(tcp::socket& socket);
+void send(tcp::socket& socket);
+
+void wait(tcp::socket& socket) {
+    auto buffer = std::make_shared<asio::streambuf>();
+
+    asio::async_read_until(socket, *buffer, '\n',
+        [&socket, buffer](const std::error_code& ec, std::size_t /*length*/) {
+            if (!ec) {
+                buffer->consume(buffer->size()); // Discards the data
+
+                send(socket);
+            } else {
+                std::cerr << "Error while reading: " << ec.message() << std::endl;
+            }
+        });
+}
+
+void send(tcp::socket& socket) {
+    std::string message;
+    std::cout << "Enter message: ";
+    std::getline(std::cin, message);
+    if (message.empty()) return;
+    message += '\n';
+    
+    asio::async_write(socket, asio::buffer(message),
+            [&](std::error_code ec, size_t /*length*/) {
+                if (!ec) {
+                    wait(socket);
+                };
+            });
+}
+
+int main() {
     try {
-        if (argc != 3) {
-            std::cerr << "Usage: client <host> <message>" << std::endl;
-            return -1;
-        }
         asio::io_context io_context;
         tcp::resolver resolver(io_context);
-        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "3000");
+        tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "3000");
 
         tcp::socket socket(io_context);
         asio::connect(socket, endpoints);
-        
-        asio::write(socket, asio::buffer(argv[2], std::strlen(argv[2])));
+
+        wait(socket);
+
+        io_context.run();
+
+        socket.close();
+        io_context.stop();
 
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
--- src/server.cpp
+++ src/server.cpp
@@ -1,34 +1,83 @@
-#include <ctime>
+#include <memory>
+#include <string>
 #include <iostream>
-#include <array>
 #include <asio.hpp>
 
 using asio::ip::tcp;
+typedef asio::strand<asio::io_context::executor_type> out_strand;
+
+class tcp_connection : public std::enable_shared_from_this<tcp_connection> {
+    tcp::socket socket_;
+    asio::streambuf buffer_;
+
+    explicit tcp_connection(tcp::socket socket) : 
+        socket_(std::move(socket)) {}
+
+    void read() {
+        asio::async_read_until(socket_, buffer_, '\n',
+            [self = shared_from_this()](std::error_code ec, std::size_t length) {
+                if (!ec) {
+                    std::istream is(&self->buffer_);
+                    std::string message;
+                    std::getline(is, message);
+                    std::cout << "Message received: " << message << std::endl;
+                    self->respond();
+                }
+            });
+    }
+
+    void respond() {
+        asio::async_write(socket_, asio::buffer("\n"),
+            [self = shared_from_this()](std::error_code ec, std::size_t /*length*/) {
+                if (!ec) {
+                    self->read();  // Continue reading
+                }
+            });
+    }
+
+public:
+    typedef std::shared_ptr<tcp_connection> pointer;
+
+    static pointer create(tcp::socket socket) {
+        return pointer(new tcp_connection(std::move(socket)));
+    }
+
+    tcp::socket& socket() { return socket_; }
+
+    void start() {
+        respond();
+    }
+};
+
+class tcp_server {
+    tcp::acceptor acceptor_;
+
+    void start_accept() {
+        acceptor_.async_accept([this](std::error_code ec, tcp::socket socket) {
+            if (!ec) {
+                std::cout << "Connection made successfully" << std::endl;
+                tcp_connection::pointer connection(tcp_connection::create(std::move(socket)));
+                connection->start();
+            }
+            start_accept();
+        });
+    }
+
+public:
+    explicit tcp_server(asio::io_context& io_context) :
+        acceptor_(io_context, tcp::endpoint(tcp::v4(), 3000)) {
+        
+        start_accept();
+    }
+};
 
 int main() {
     try {
         asio::io_context io_context;
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 3000));
-
-        for (;;) {
-            tcp::socket socket(io_context);
-            acceptor.accept(socket);
-            
-            while (true) {
-                std::error_code err;
-                std::array<char, 128> buf;
-                size_t len = socket.read_some(asio::buffer(buf), err);
-
-                if (err == asio::error::eof)
-                    break;
-                else if (err)
-                    throw std::system_error(err);
-
-                std::cout << buf.data() << std::endl;
-                std::cout.flush();
-            }
-        }
+        tcp_server server(io_context);
+        io_context.run();
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
+    return 0;
 }
